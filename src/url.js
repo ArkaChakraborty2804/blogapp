bfc62dc46c12079065a49b9b35e5be2b0d4b4d7b
Arka Chakraborty <arkachakraborty@Arkas-Air.mshome.net>
2024-01-10 21:24:04 +0530

    backend url added

--- src/url.js
+++ src/url.js
@@ -1,3 +1,3 @@
 // export const URL=import.meta.env.VITE_URL
 export const IF=import.meta.env.VITE_IF
-export const URL = 'http://localhost:5000'
+export const URL = 'https://blogger-backend1.onrender.com'
